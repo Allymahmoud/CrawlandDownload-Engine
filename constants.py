48696cfd16240bbdd7aca4fe8737857e2b367d8d
Ally Mahmoud <ally.h.mahmoud.19@dartmouth.edu>
2017-08-05 19:12:49 +0200

    added a crawler function to crawl

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@
 # This python file is for storing constants
 
 
-websiteUrl = "http://mfma.treasury.gov.za/Documents/Forms/AllItems.aspx"
+websiteUrl = "http://mfma.treasury.gov.za/Documents/Forms/AllItems.aspx?RootFolder=%2fDocuments%2f04%2e%20Service%20Delivery%20and%20Budget%20Implementation%20Plans%2f2008-09%2f01%2e%20Metros%2fEC000%20Nelson%20Mandela%20Bay&FolderCTID=&View=%7b84CA1A01-EF8A-4DE0-8DC4-47D223CB5867%7d"
 baseURL = "http://mfma.treasury.gov.za/Documents"
 fileExtension = ".pdf"
+depth = 20
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import urllib2
 import urllib
 import re
 import constants
+import  Queue
 
 
 #connect to a URL
@@ -57,9 +58,67 @@ for link in links:
 
         urllib.urlretrieve(link[0],filtername)
 
-    if link[0].startswith(constants.websiteUrl):
+    if link[0].startswith(constants.baseURL):
         i+=1
         print (i, link[0])
 
 print customUrlParser(sampleUrl)
+
+#method to convert list of tuples to a list of string
+def listTuplesTolistStrings(listTuples):
+    listStrings = []
+    for tuple in listTuples:
+        listStrings.append(tuple[0])
+
+    return listStrings
+
+def crawler(depth=constants.depth, baseurl = constants.baseURL, seedUrl = constants.websiteUrl, fileExtension = constants.fileExtension):
+    pagesToVisit = [seedUrl]
+    visitedPages = []
+
+    currentDepth = 0
+    counter = 1
+    while currentDepth < depth and pagesToVisit != []:
+        print len(pagesToVisit)
+
+        currentUrl = pagesToVisit.pop(0)
+
+        print currentUrl
+
+        if (not currentUrl in visitedPages) and currentUrl.startswith(baseurl):
+            visitedPages.append(currentUrl)
+
+            if currentUrl.endswith(fileExtension):
+                counter+=1
+                print str(counter) + " found pdf document: " + currentUrl
+            else:
+                # connect to a URL
+                website = urllib2.urlopen(currentUrl)
+
+                # read html code
+                html = website.read()
+
+                # use re.findall to get all the links
+                links = re.findall('"((http|ftp)s?://.*?)"', html)
+
+                # add the new urls to pagesToVisit
+                pagesToVisit.extend(listTuplesTolistStrings(links))
+
+
+
+
+list = [1, 2, 3, 4]
+
+if not 5 in list:
+    print "not in list"
+
+
+
+
+
+
+
+
+
+
 
